835c29aa0d3d65bffda0157db1122de4f673ddb0
Immortalyzy <immortalyzy@gmail.com>
2023-12-15 14:39:49 +0100

    fix directory name error.

--- GameResrouces/OriginalFiles/file_path.py
+++
@@ -1,6 +0,0 @@
-""" For big games, it might be unwise to store all the files under this folder. This file is used to store the path of the files. """
-
-
-GAME_PATH = "U:\\Toys\\Games\\Gal\\神聖昂燐i\\"
-GAME_ENGINE = "Kirikiri2"
-MAIN_FUNCTION = "神聖昂燐ダクリュオン ルナ.exe"
