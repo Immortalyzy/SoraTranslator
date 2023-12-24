df6dc216bbb1a272b0c17d33534463a95270dd6a
MrVergil <immortalyzy@gmail.com>
2023-12-24 11:10:28 +0100

    Add a table.

--- frontend/src/components/FileDisplay.vue
+++ frontend/src/components/FileDisplay.vue
@@ -1,37 +1,58 @@
 <template>
-  <div class="file-display">
-    <pre>{{ text }}</pre>
-    <div class="actions">
-      <button>Edit</button>
-      <button>Delete</button>
-      <button>Save</button>
-      <!-- Add more buttons as needed -->
-    </div>
+  <div class="table-container">
+    <table>
+      <thead>
+        <tr>
+          <th v-for="header in headers" :key="header">{{ header }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, rowIndex) in tableData" :key="rowIndex">
+          <td v-for="(cell, cellIndex) in row" :key="cellIndex" contenteditable @blur="editCell(rowIndex, cellIndex, $event)">
+            {{ cell }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FileDisplay',
-  props: {
-    text: String
-  }
+  data() {
+    return {
+      headers: ['Name', 'Age', 'Occupation'],
+      tableData: [
+        ['Alice', 28, 'Engineer'],
+        ['Bob', 34, 'Designer'],
+        // ... more rows
+      ],
+    };
+  },
+  methods: {
+    editCell(rowIndex, cellIndex, event) {
+      this.tableData[rowIndex][cellIndex] = event.target.innerText;
+    },
+  },
 };
 </script>
 
-<style scoped>
-.file-display {
-  flex: 3;
-  background-color: #fff;
-  padding: 10px;
-  overflow-y: auto;
+<style>
+.table-container {
+  overflow: auto;
+  max-height: 300px; /* Adjust as needed */
 }
 
-.actions {
-  margin-top: 10px;
+table {
+  width: 100%;
+  border-collapse: collapse;
 }
 
-button {
-  margin-right: 5px;
+th, td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
 }
+
+/* Additional styling as needed */
 </style>
